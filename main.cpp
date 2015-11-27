70ed27eedd148bddd2c8722cf2f346b939d93bf0
Victor Gressier <victor15@ru.is>
2015-11-27 14:36:03 +0000

    MENU

--- main.cpp
+++ main.cpp
@@ -6,17 +6,19 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    cout << "Hello world" << endl;
-    cout << "Hello friends &co";
 
     int choice = 0;
     cout << "Welcome to Computer Sciense DB." << endl;
-    while(choice != 4){
-        cout << "1: Insert\n2: Print\n3: Delete\n4: Exit\nEnter your choice:";
+    do{
+        cout << "1: Insert" << endl
+             << "2: Print" << endl
+             << "3: Delete" << endl
+             << "4: Exit\n" << endl
+             << "Enter your choice:" << endl;
         cout.flush();
         cin >> choice;
         cout << choice;
-    }
+    }while(choice != 4);
 
     return a.exec();
 }
