9dc17391082d30e826a48beda416bf977dcbdaf8
Victor Gressier <victor15@ru.is>
2015-11-27 17:16:51 +0000

    day 1

--- main.cpp
+++ main.cpp
@@ -14,27 +14,30 @@ int main(int argc, char *argv[])
     fin.open("database.csv");
     fout.open("database.csv");
 
-    list theList();
+    List theList(fin);
 
     int choice;
     cout << "Welcome to Computer Sciense DB." << endl;
     do{
         cout << "1: Insert" << endl
-             << "2: Print" << endl
-             << "3: Delete" << endl
-             << "4: Exit" << endl
+             << "2: Search" << endl
+             << "3: Print" << endl
+             << "4: Delete" << endl
+             << "5: Exit" << endl
              << "Enter your choice:" << endl;
         cout.flush();
         cin >> choice;
 
-        if (choice == 1);
+        if (choice == 1)
+            theList.InsertPerson();
         else if (choice == 2);
         else if (choice == 3);
         else if (choice == 4);
+        else if (choice == 5);
         else
             cout << "Invalid choice!" << endl;
 
-    }while(choice != 4);
+    }while(choice != 5);
 
     return a.exec();
 }
