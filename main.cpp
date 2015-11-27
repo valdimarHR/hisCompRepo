6008a8bfeb7400e57bc1f085171d5f860af5f555
Valdimar Baldvinsson <valdimar14@ru.is>
2015-11-27 13:36:05 +0000

    add CSV file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     //Merge purfa
     QCoreApplication a(argc, argv);
+
+    int choice = 0;
+    cout << "Welcome to Computer Sciense DB." << endl;
+    while(choice != 4){
+        cout << "1: Insert\n2: Print\n3: Delete\n4: Exit\nEnter your choice:";
+        cout.flush();
+        cin >> choice;
+        cout << choice;
+    }
 
     return a.exec();
 }
