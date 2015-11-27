2840498b622b1e2fe02a1cb71ea9d4ce90080dbb
huldah15 <huldah15@ru.is>
2015-11-27 11:52:29 +0000

    hreinsa

--- main.cpp
+++ main.cpp
@@ -2,18 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv); //victortest
-    //Merge purfa
     QCoreApplication a(argc, argv);
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //prufa
-=======
->>>>>>> d178d48a179384487ee608bf68a1ca067f9d6034
->>>>>>> cdaf1b9463c2cdf891d380474b5b287d29d14f77
-=======
 
->>>>>>> 83d1dae908cff5add3e5649b4afb594d3ccfcce6
 
     return a.exec();
 }
