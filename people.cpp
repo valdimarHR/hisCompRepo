9cde80535449d6edb587b59810ab50a21d4d66f2
embla15 <embla15@ru.is>
2015-11-28 14:14:42 +0000

    Breytingar í people cpp frá Huldu

--- people.cpp
+++ people.cpp
@@ -7,3 +7,43 @@ people::people()
 
 }
 
+void people::setName(string fname)
+{
+    name = fname;
+}
+
+void people::setGender(string fgender)
+{
+    gender = fgender;
+}
+
+void people::setBirth(int fbirth)
+{
+    yearOfBirth = fbirth;
+}
+
+void people::setDeath(int fdeath)
+{
+    yearOfDeath = fdeath;
+}
+
+string people::getName()
+{
+    return name;
+}
+
+string people::getGender()
+{
+    return gender;
+}
+
+int people::getBirth()
+{
+    return yearOfBirth;
+}
+
+int people::getDeath()
+{
+    return yearOfDeath;
+}
+
