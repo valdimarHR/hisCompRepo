8f1905323053403686b065ac3abe84f88f2a4864
huldah15 <huldah15@ru.is>
2015-12-01 12:57:13 +0000

    Hreinsaði líka í lokinn á insert

--- list.cpp
+++ list.cpp
@@ -1,5 +1,4 @@
 #include "list.h"
-#include <iostream>
 
 using namespace std;
 
@@ -132,11 +131,16 @@ void List::InsertPersonInfo(ostream& outs)
     {
         cout << endl << "This person was already on the list and was therefore not added again."
              << endl << endl;
+        sleep(3);
+        system("cls");
         return;
     }
 
     outs << Name << ", " << gender << ", " << born << ", " << death << endl;
     listOfPeople.push_back(per);
+    cout << endl << "Person was added to the list.";
+    sleep(2);
+    system("cls");
 }
 
 bool List::checkIfpersonOnList(const people &person)
