90cb5d70d7dd7f05a7e7e7c9c7e9646eb57f9911
Victor Gressier <victor15@ru.is>
2015-12-05 13:19:36 +0000

    computers get og set

--- Models/computers.cpp
+++ Models/computers.cpp
@@ -2,6 +2,44 @@
 
 Computers::Computers()
 {
-
+    name = "default";
+    yearCreated = 0;
+    type = "default";
+    wasBuilt = false;
 }
 
+void Computers::setName(string fname)
+{
+    name = fname;
+}
+
+void Computers::setYearCreated(int year)
+{
+    yearCreated = year;
+}
+void Computers::setType(string ftype)
+{
+    type = ftype;
+}
+
+void Computers::setWasBuilt(bool built)
+{
+    wasBuilt = built;
+}
+
+string Computers::getName() const
+{
+    return name;
+}
+int Computers::getYearCreated() const
+{
+    return yearCreated;
+}
+string Computers::getType() const
+{
+    return type;
+}
+bool Computers::getWasBuilt() const
+{
+    return wasBuilt;
+}
--- Models/computers.h
+++ Models/computers.h
@@ -1,11 +1,26 @@
 #ifndef COMPUTERS_H
 #define COMPUTERS_H
-
+#include <iostream>
+using namespace std;
 
 class Computers
 {
 public:
     Computers();
+    void setName(string name);
+    void setYearCreated(int year);
+    void setType(string type);
+    void setWasBuilt(bool built);
+    string getName() const;
+    int getYearCreated() const;
+    string getType() const;
+    bool getWasBuilt() const;
+private:
+    int id;
+    string name;
+    int yearCreated;
+    string type;
+    bool wasBuilt;
 };
 
 #endif // COMPUTERS_H
