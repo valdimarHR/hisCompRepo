c81c26e38a2879b864a3d5f4c4203bd9136f2f83
embla15 <embla15@ru.is>
2015-12-08 20:52:05 +0000

    Kommentin tilbúin

--- UI/ui.h
+++ UI/ui.h
@@ -27,19 +27,21 @@ private:
     void insertMenuPerson();//Asks for info of the person that is to be added.
     void insertMenuComputer();//Asks for info of the computer that is to be added.
     void insertMenuConnection();
-    /*
-     * searchMenu asks the user whether it wants to search for a person or a computer and goes to
+    /**
+     * @brief asks the user whether it wants to search for a person or a computer and goes to
      * the appropriate function
      */
     void searchMenu();
-    /*
-     * searchMenuPerson asks the user in which way it wants to search for a person and gives the options of name,
-     * gender, year of birth and year of death and returns the person the user is searching for
+    /**
+     * @brief asks the user in which way it wants to search for a person and gives the options of name,
+     * gender, year of birth and year of death
+     * @return returns the person or people that matches the user's search
      */
     void searchMenuPerson();
-    /*
-     * searchMenuComputer asks the user in which way it wants to search for a computer and gives the options of name,
-     * type, year of creation and whether or not it was built and returns the computer the person is searching for
+    /**
+     * @brief asks the user in which way it wants to search for a computer and gices the options of name,
+     * type, year of creation and whether or not it was built
+     * @return returns the computer(s) that matches the user's search
      */
     void searchMenuComputer();
     void printerMenu();
