a2c99cd8da54936c58194432d9bcdd83cf4df743
Valdimar Baldvinsson <valdimar14@ru.is>
2015-12-08 22:14:37 +0000

    added ui comments

--- UI/ui.h
+++ UI/ui.h
@@ -8,8 +8,8 @@
 #include <QtGlobal>
 
 /**
- * @brief The UI prints out display, gets information from the use to
- * send to logic. Connects the user to the logic. Checks if the input
+ * @brief The UI prints out to the display and gets information from the user and
+ * send it to the logic layer. Connects the user to the logic. Checks if the input
  * is correct.
  */
 
@@ -21,7 +21,13 @@ public:
     ui();
     bool start();
     void printTree() const;
+    /*
+     * printPeopleVector() prints out the people and their computers as a table to the display.
+     */
     void printPeopleVector(const vector<peopleWithComputers>& list) const;
+    /*
+     * printComputersVector() prints out the computers and the people that created them as a table to the display.
+     */
     void printComputersVector(const vector<computersWithPeople>& list) const;
 private:
     logic theLogic;
@@ -47,11 +53,29 @@ private:
      * type, year of creation and whether or not it was built and returns the computer the person is searching for
      */
     void searchMenuComputer();
+    /*
+     * printerMenu() is the main UI for printing the entire data in the database without any search requirements.
+     * Print also offers sorting of the data as the user likes.
+     */
     void printerMenu();
+    /*
+     * printerMenuPeople() is the UI for printing all the people in the database.
+     */
     void printerMenuPeople();
+    /*
+     * printerMenuComputers() is the UI for printing all the computers in the database.
+     */
     void printerMenuComputers();
     void deleteMenu();//Asks the user what attribute they want to delete(Scientist, computer or all).
+    /*
+     * inputIntCheck(bool, int&) checks if the input from the user was an integer. If not it will ask the user to
+     * try again.
+     */
     void inputIntCheck(bool inputFail, int& var);
+    /*
+     * inputIntCheck(bool, int&, int, int) checks if the input from the user was an integer and within the right range.
+     * If not it will ask the user to try again.
+     */
     void inputIntCheck(bool inputFail, int& var, int low, int high);
     void deletePeople();//Asks the user what scientist they wish to remove.
     void deleteComputer();//Asks the user what computer they wish to remove.
@@ -60,6 +84,9 @@ private:
     int printOnlyComputers(const vector<computersWithPeople>& comp);//Simple print to screen only list of computers.
     string getStringSearchValue(string question);
     bool inputStrToBool(string& built);
+    /*
+     * clear() sents a clear command to the console dependant on the user OS.
+     */
     void clear() const;
 };
 
