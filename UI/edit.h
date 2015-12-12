a30bc8eb97bc175606925f7b3897882ad37bc8a7
huldah15 <huldah15@ru.is>
2015-12-12 16:40:08 +0000

    bjó til glugga fyrir edit people

---
+++ UI/edit.h
@@ -0,0 +1,34 @@
+#ifndef EDIT_H
+#define EDIT_H
+#include <mainwindow.h>
+#include <Models/peoplewithcomputers.h>
+#include <string>
+#include <QMessageBox>
+#include "Processor/logic.h"
+
+#include <QDialog>
+
+namespace Ui {
+class Edit;
+}
+
+class Edit : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Edit(QWidget *parent = 0);
+    ~Edit();
+
+private slots:
+    void on_pushButtonSubmit_clicked();
+
+
+private:
+    Ui::Edit *ui;
+    MainWindow mainwindow;
+    logic TheLogic;
+    displayPerson();
+};
+
+#endif // EDIT_H
