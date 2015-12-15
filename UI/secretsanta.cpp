cb663f633802b5c6843490015eb6e3b9fb9bd405
huldah15 <huldah15@ru.is>
2015-12-15 13:27:07 +0000

    secretsanta

---
+++ UI/secretsanta.cpp
@@ -0,0 +1,20 @@
+#include "secretsanta.h"
+#include "ui_secretsanta.h"
+#include <QPixmap>
+
+secretSanta::secretSanta(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::secretSanta)
+{
+    ui->setupUi(this);
+}
+
+secretSanta::~secretSanta()
+{
+    delete ui;
+}
+
+void secretSanta::on_ButtonChristmas_clicked()
+{
+    this->close();
+}
